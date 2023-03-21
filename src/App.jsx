5de903c7139c058f03a09bfbbb90018c84527ca9
JonathanEKP <jonathanvaldez2001@gmail.com>
2023-03-21 12:40:44 -0600

    Carpeta raiz con vite.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <p className='text-dark'>Holaaa</p>
+      <button className='btn btn-outline-danger' type='button'>XDDDDDD</button>
+    </div>
+  )
+}
+
+export default App
